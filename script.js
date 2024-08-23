efb3456cc60f0cfc9735f51c1b763e5e9920653a
varunkewlani <91694985+varunkewlani@users.noreply.github.com>
2024-08-23 19:13:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//your JS code here. If required.
+let c = document.getElementById("btn");
+
+c.addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    let a = document.getElementById("age").value;
+    let b = document.getElementById("name").value;
+    
+    let promise1 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(a >= 18) {
+                resolve(`Welcome, ${b}. You can vote`);	
+            } else if(a < 18) {
+                reject(`Oh sorry ${b}. You aren't old enough.`);
+            }
+        }, 4000);
+    });
+    
+    promise1.then((data) => {
+        alert(data);
+    }).catch((err) => {
+        alert(err);
+    });
+});
